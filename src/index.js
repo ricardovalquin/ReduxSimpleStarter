ea6dd7366fa0b69ee51fc04fdab3f9da415f52a5
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-07-26 08:02:16 -0500

    changed App component from functional to class based component due to the necessity to handle state to use the Youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/Search-bar';
 
 const API_KEY = 'AIzaSyAIhEBWyOfetev7IS0Ro5B-DYwPX0AxAmU';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-ReactDom.render(<App />, document.querySelector('.container'));
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      console.log(data);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+
+ReactDom.render(
+  <App />,
+  document.querySelector('.container')
+);
