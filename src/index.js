41f3bd1a5810e9e16b8827c13f2d634adf84ae84
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-07-26 10:36:19 -0500

    sending a function to video lit item component to change the video to reproduce

--- src/index.js
+++ src/index.js
@@ -13,11 +13,15 @@ class App extends Component {
     super(props);
 
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -25,8 +29,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={ this.state.videos[0] }/>
-        <VideoList videos={ this.state.videos } />
+        <VideoDetail video={ this.state.selectedVideo }/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+          videos={ this.state.videos }
+        />
       </div>
     );
   }
